115c576fc2431c75e61072797d38739afa8a2613
Anish Mukherjee <cybertronian217@gmail.com>
2022-03-25 10:48:27 +0530

    Added button to call contract and mint NFT

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 import React, { useEffect, useState } from "react";
+import { ethers } from 'ethers';
+import myEpicNft from './utils/MyEpicNFT.json'
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
@@ -21,7 +23,7 @@ const App = () => {
       console.log("We have the ethereum object", ethereum)
     }
 
-    const accounts = await ethereum.request({method: 'eth_accounts'});
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
 
     if (accounts.length !== 0) {
       const account = accounts[0];
@@ -33,17 +35,44 @@ const App = () => {
   }
 
   const connectWallet = async () => {
-    try{
+    try {
       const { ethereum } = window
-      if (!ethereum){
+      if (!ethereum) {
         alert("Get Metamask!")
         return
       }
-      const accounts = await ethereum.request({method:"eth_requestAccounts"});
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
 
-      console.log("Connected",accounts[0])
+      console.log("Connected", accounts[0])
       setCurrentAccount(accounts[0])
-    } catch(error){
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const askContractToMintNft = async () => {
+    const CONTRACT_ADDRESS = "0x1D1752e7defc4f384C1381363fbcf03450AA0eaB";
+
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+
+        console.log("Going to pop wallet now to pay gas...")
+        let nftTxn = await connectedContract.makeAnEpicNFT();
+
+        console.log("Mining...please wait.")
+        await nftTxn.wait();
+
+        console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
       console.log(error)
     }
   }
@@ -70,8 +99,8 @@ const App = () => {
           </p>
           {currentAccount === "" ? (
             renderNotConnectedContainer()
-          ):(
-            <button onClick={null} className="cta-button connect-wallet-button">
+          ) : (
+            <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
               Mint NFT
             </button>
           )}
